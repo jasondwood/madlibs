dfac07bebbd50ffc42d027311ec859af08407ec3
jasondwood <823jwood@gmail.com>
2022-06-01 07:49:08 -0400

    added form

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 
+let btn = document.querySelector('button');
+let box = document.querySelector('#box');
+
+btn.addEventListener('click',() => {
+    box.click();
+})
+box.addEventListener('click', (e) => {
+    box.style.backgroundColor = "green";
+})
